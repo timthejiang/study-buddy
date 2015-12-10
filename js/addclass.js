91d43bc29df721a363f9459ca497ceeaac260fa3
csanjana <chndrsh4@illinois.edu>
2015-12-10 14:05:36 -0600

    Modified the adding new class functionality

--- js/addclass.js
+++ js/addclass.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
     // $('.status').text(this.innerHTML);
     // alert(this.innerHTML);
     $("#added-classes").prepend('<li><a href="empty_class_page.html">'+this.innerHTML+'</a></li>');
+    $(".sel-class").text(this.innerHTML);
+    $(".sel-class").attr("href","./empty_class_page.html");
     $(".close-modal").click();
 });
 });
